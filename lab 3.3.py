9965afe4f5ee7d98a308db8c8ac8b9ac1d34ce6e
jenil4064 <jenilparsaniya5012@gmail.com>
2025-02-08 16:00:30 +0530

    Update lab 3.3.py

--- lab 3.3.py
+++ lab 3.3.py
@@ -1,8 +1,12 @@
-string = str(input("enter your string"))
-
-for i in range( 0,len(string) ):
-               
-    print(string[i])
-    
-    
-    
+string = input("enter a string here:")
+alpha = 0
+digits = 0
+for char in string:
+    if char.isalpha():
+       alpha = alpha+1
+        
+    elif char.isdigit():
+        digits = digits+1
+       
+print("No. of alphabets :", alpha)
+print("No. of digits :", digits)
