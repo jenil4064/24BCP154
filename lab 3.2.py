48fca584667f567fae7714de02402a9ae591921b
jenil4064 <jenilparsaniya5012@gmail.com>
2025-02-08 16:00:09 +0530

    Update lab 3.2.py

--- lab 3.2.py
+++ lab 3.2.py
@@ -1,4 +1,6 @@
-a = int(input("enter your number"))
-for i in range(1,11):
-    print(a, "X" , i ,
-          "=" , a * i)
+a = int(input("Enter a number to print :"))
+i = 1
+
+while i < 11:
+    print(a * i)
+    i += 1
