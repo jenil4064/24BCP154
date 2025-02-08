fd36b969acb60f3741c232cc5185ccee5ecfc530
jenil4064 <jenilparsaniya5012@gmail.com>
2025-02-08 16:03:47 +0530

    Update and rename LAB 3.8.py to lab 3.8.py

--- LAB 3.8.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter value of n: "))
-
-fect_n = 1
-for i in range(1, n + 1):
-    fect_n *= i
-if n < 0:
-    print("Factorial is not defined for negative numbers.")
-else:
-    print(f"The factorial of {n} is {fect_n}")
